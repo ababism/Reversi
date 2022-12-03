3315df0ed34668c27f84db30d4d2876d2b6d2eda
Ivan Kiselev <kiselev.contact@gmail.com>
2022-12-03 21:42:52 +0300

    version with huge mistakes

--- src/main/java/com/game/reversi/Board.java
+++ src/main/java/com/game/reversi/Board.java
@@ -5,24 +5,9 @@ import java.util.Arrays;
 
 public class Board {
     final static public int BOARD_SIZE = 8;
-    final static private String BOARD_FORMAT = """
-            -------------------------
-            |%c|%c|%c|%c|%c|%c|%c|%c|
-            -------------------------
-            |%c|%c|%c|%c|%c|%c|%c|%c|
-            -------------------------
-            |%c|%c|%c|%c|%c|%c|%c|%c|
-            -------------------------
-            |%c|%c|%c|%c|%c|%c|%c|%c|
-            -------------------------
-            |%c|%c|%c|%c|%c|%c|%c|%c|
-            -------------------------
-            |%c|%c|%c|%c|%c|%c|%c|%c|
-            -------------------------
-            |%c|%c|%c|%c|%c|%c|%c|%c|
-            -------------------------
-            |%c|%c|%c|%c|%c|%c|%c|%c|
-            """;
+    private Chip playerColor;
+    private Chip opponentsColour;
+
     private final Chip[][] desk = new Chip[BOARD_SIZE][BOARD_SIZE];
 
     public Board() {
@@ -63,10 +48,11 @@ public class Board {
             return '◍';
         } else if (chip == Chip.BLACK) {
             return '◯';
-        } else {
+        } else if (chip == null &&) {
             return '_';
         }
     }
+
     void placeChipAt(Chip color, int x, int y) throws IllegalArgumentException {
         if (x >= BOARD_SIZE || x < 0 || y >= BOARD_SIZE || y < 0) {
             throw new IllegalArgumentException("Wrong chip coordinates");
@@ -76,19 +62,83 @@ public class Board {
     }
 
     //TODO
-    boolean isChipPlaceableAt(Chip color, int x, int y) {
+    boolean isChipPlaceableAt(int x, int y) {
         if (x >= BOARD_SIZE || x < 0 || y >= BOARD_SIZE || y < 0) {
             return false;
         }
+        for (var i = x + 1; i < x; ++i) {
+            if (desk[x][i] == playerColor) {
+
+            }
+        }
+        if
         //TODO
         return false;
     }
+    // сликшом много повторяющего кода
+    boolean coordinateInRange(int a) {
+        return a >= 0 && a < BOARD_SIZE;
+    }
 
+    boolean chipFlipsLeftRow(int x, int y) {
+        int it = x - 1;
+        if (!coordinateInRange(it)) {
+            return false;
+        }
+        while (desk[it][y] == opponentsColour && it > 0) {
+            --it;
+        }
+        return desk[it][y] == playerColor;
+    }
+
+    boolean chipFlipsRightRow(int x, int y) {
+        int it = x + 1;
+        if (!coordinateInRange(it)) {
+            return false;
+        }
+        while (desk[it][y] == opponentsColour && it < BOARD_SIZE - 2) {
+            ++it;
+        }
+        return desk[it][y] == playerColor;
+    }
+
+    boolean chipFlipsUpColumn(int x, int y) {
+        int it = y - 1;
+        if (!coordinateInRange(it)) {
+            return false;
+        }
+        while (desk[x][it] == opponentsColour && it > 0) {
+            --it;
+        }
+        return desk[x][it] == playerColor;
+    }
+
+    boolean chipFlipsBottomColumn(int x, int y) {
+        int it = y + 1;
+        if (!coordinateInRange(it)) {
+            return false;
+        }
+        while (desk[x][it] == opponentsColour && it < BOARD_SIZE - 2) {
+            --it;
+        }
+        return desk[x][it] == playerColor;
+    }
+
+    boolean chipFlipsFirstDiagonal(int x, int y) {
+        int it = y - 1;
+        if (!coordinateInRange(it)) {
+            return false;
+        }
+        while (desk[x][it] == opponentsColour && it > 0) {
+            --it;
+        }
+        return desk[x][it] == playerColor;
+    }
     public void DisplayBoard() {
 //        System.out.println("_________________");
         System.out.println("_|1|2|3|4|5|6|7|8|");
         for (int i = 0; i < 8; i++) {
-            System.out.print(i+1);
+            System.out.print(i + 1);
             System.out.printf("|%c|%c|%c|%c|%c|%c|%c|%c|\n", Arrays.stream(desk[i]).map(Chip::chipToChar).toArray());
         }
 //        for (var chipArr : desk) {
